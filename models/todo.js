9b651d2d0586d6a98673ea2c04069030bd1df67c
josuecedeno <josuecedeno@yahoo.com>
2021-02-23 13:07:28 -0600

    Update readme

--- models/todo.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const TodoSchema = new Schema({
-  text: String,
-});
-
-module.exports = mongoose.model('Todo', TodoSchema);
--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-
-const app = express();
-
-mongoose.connect('mongodb://localhost:27017');
-
-app.listen(8080, () => console.log('Running on port 8080'));
